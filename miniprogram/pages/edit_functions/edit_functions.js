568c0d0889dc524b1e017456bdfe775bdbba0554
L qx <pokethislucien@qq.com>
2020-04-15 23:51:19 +0800

    emerge01

--- miniprogram/pages/edit_functions/edit_functions.js
+++ miniprogram/pages/edit_functions/edit_functions.js
@@ -1,4 +1,5 @@
 // pages/edit_functions/edit_functions.js
+import CanvasDrag from '../../components/canvas-drag/canvas-drag';
 const app = getApp()
 Page({
 
@@ -6,130 +7,185 @@ Page({
    * 页面的初始数据
    */
   data: {
-    imgPath: null,
-    addIconList: [
+    cWidth: 0,
+    cHeight: 0, // canvas的完整高度
+    textToPrint:'',
+    graph: {},
+    btnInfo: [
       {
-        icon: "star",
-        color: 'black',
-        size: 25,
-        name: '从收藏中添加'
-      }
+        type: 'text',
+        background: 'url("../../images/text.png") white no-repeat; background-size: 20px 20px;background-position: 2px 2px;'
+      },
+      {
+        type: 'paint',
+        background: 'url("../../images/paint.png") white no-repeat; background-size: 20px 20px;background-position: 2px 2px;'
+      },
+      {
+        type: 'fliter',
+        background: 'url("../../images/fliter.png") white no-repeat; background-size: 20px 20px;background-position: 2px 2px;'
+      },
+      {
+        type: 'save',
+        background: 'url("../../images/save.png") white no-repeat; background-size: 20px 20px;background-position: 2px 2px;'
+      },
     ],
-    existIconList: [
-      {
-        icon: "add2",
-        color: 'black',
-        size: 25,
-        name: '添加到收藏'
-      },
-      {
-        icon: "delete",
-        color: 'black',
-        size: 25,
-        name: '删除'
-      }
-    ],
-    iconList: [
-      {
-        icon: "add-friends",
-        color: 'black',
-        size: 25,
-        name: ''
-      },
-      {
-        icon: "add",
-        color: 'black',
-        size: 25,
-        name: ''
-      },
-      {
-        icon: "add2",
-        color: 'black',
-        size: 25,
-        name: ''
-      },
-      {
-        icon: "album",
-        color: 'black',
-        size: 25,
-        name: ''
-      },
-      {
-        icon: "arrow",
-        color: 'black',
-        size: 12,
-        name: ''
-      },
-      {
-        icon: "at",
-        color: 'black',
-        size: 25,
-        name: ''
-      },
-      {
-        icon: "back",
-        color: 'black',
-        size: 12,
-        name: ''
-      },
-      {
-        icon: "back2",
-        color: 'black',
-        size: 25,
-        name: ''
-      },
-      {
-        icon: "bellring-off",
-        color: 'black',
-        size: 25,
-        name: ''
-      },
-      {
-        icon: "bellring-on",
-        color: 'black',
-        size: 25,
-        name: ''
-      },
-      {
-        icon: "camera",
-        color: 'black',
-        size: 25,
-        name: ''
-      },
-      {
-        icon: "cellphone",
-        color: 'black',
-        size: 25,
-        name: ''
-      },
-      {
-        icon: "clip",
-        color: 'black',
-        size: 25,
-        name: ''
-      },
-      {
-        icon: "close",
-        color: 'black',
-        size: 25,
-        name: ''
-      }]
+    text: false,
+    maxLen: false,
+    paint: false,
+    r: 33,
+    g: 33,
+    b: 33,
+    w: 10,
+    colorArrF: ['black', 'whitesmoke', 'red', 'orange', 'yellow'],
+    colorArrS: ['green', 'blue', 'cyan', 'purple', 'gray'],
+    currentColor: 'black',
   },
 
-  handle: function (e) {
-    console.log(e.currentTarget.dataset.index)
-    var name = e.currentTarget.dataset.index
-    if (name == "从收藏中添加") {
-      this.setData({
-        imgPath: app.globalData.tempUrl
-      })
-    }
-    if (name == "删除") {
-      this.setData({
-        imgPath: null
-      })
+  onTabItemTap() {
+    let that = this
+    wx.getSystemInfo({
+      success (res) {
+        that.setData({
+          cWidth: res.windowWidth,
+          cHeight: res.windowHeight/2,
+        })
+      }
+    })
+    wx.chooseImage({
+      count: 1,
+      sizeType: ['original'],
+      sourceType: ['album', 'camera'],
+      success (res) {
+        console.log(res.tempFilePaths[0])
+        wx.getImageInfo({
+          src: res.tempFilePaths[0],
+          success (res) {
+            var rate = res.height/res.width
+            console.log(res.path)
+            that.setData({
+              graph: {
+                w: 200,
+                h: 200*rate,
+                type: 'image',
+                url: res.path,
+              }
+            })
+          //   let width = 0
+          //   let height = 0
+          //   //let ctx = wx.createCanvasContext('edit')
+          //   if (res.width > res.height) {
+          //     width = that.data.cWidth-10
+          //     height = Math.trunc(res.height/res.width*width)
+          //     //ctx.drawImage(res.path, 5, (that.data.cHeight-height)/2, width, height)
+          //   } else {
+          //     height = that.data.cHeight-10
+          //     width = Math.trunc(res.width/res.height*height)
+          //     //ctx.drawImage(res.path, (that.data.cWidth-width)/2, 5, width, height)
+          //   }
+          //  //ctx.draw()
+          //   console.log(width)
+          //   console.log(height)
+          //   that.setData({
+          //     graph: {
+          //       w: width,
+          //       h: height,
+          //       type: 'image',
+          //       url: res.path
+          //     }
+          //   })
+          },
+          fail(err){
+            console.log(err)
+          }
+        })
+      }
+    })
+  },
+
+  onAddText() {
+    var that = this
+    var obj = that.data;
+    that.setData({
+      graph: {
+        type: 'text',
+        text: obj.textToPrint,
+      }
+    });
+  },
+
+  touchStart: function() {
+    this.setData({
+      text: false,
+      paint: false,
+    })
+  },
+
+  tapBtn: function(e) {
+    let btnType = e.target.dataset.type
+    switch (btnType) {
+      case 'text':
+        this.setData({
+          text: !this.data.text,
+          paint: false
+        })
+        break;
+      case 'paint':
+        this.setData({
+          text: false,
+          paint: !this.data.paint,
+        })
     }
   },
+
+  changeColor: function(e) {
+    let temp = {}
+    temp[e.target.dataset.color] = e.detail.value
+    this.setData({
+      ...temp,
+    })
+  },
+
+  changeWidth: function(e) {
+    this.setData({
+      w: e.detail.value,
+    })
+  },
+
+  textFinish(e) {
+    let text = e.detail.value
+    let that = this
+    if(text.length > 0){
+      that.setData({
+        maxLen: true
+      })
+    } else {
+      that.setData({
+        maxLen: false
+      })
+    }
+    that.setData({
+      textToPrint: text
+    })
+  },
+
+  onAddText() {
+    this.setData({
+      graph: {
+        type: 'text',
+        text: this.data.textToPrint,
+      }
+    })
+  },
+
+  chooseColor(e) {
+    console.log(e)
+    let indexNum = e.currentTarget.id
+    this.setData({
+      currentColor: indexNum
+    })
+    CanvasDrag.changFontColor(indexNum)
+  },
+
   /**
    * 生命周期函数--监听页面加载
    */
