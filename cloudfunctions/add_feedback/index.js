0073c5141632adeb66816d51e7ff9512672ac7ec
黄继辉 <17373407@buaa.edu.cn>
2020-04-22 14:06:23 +0800

    feedback

---
+++ cloudfunctions/add_feedback/index.js
@@ -0,0 +1,30 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init({
+  env:"pyb-database-n2c6s"
+})
+
+const db = cloud.database()
+const _ = db.command
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  const wxContext = cloud.getWXContext()
+  var f = event.function_advise
+  var u = event.usage_advise
+  var o = event.other_advise
+  var s = event.score
+  var time = event.time
+  await db.collection('feedback').add({
+    data:{
+      openid: wxContext.OPENID,
+      appid: wxContext.APPID,
+      function_advise:f,
+      usage_advise:u,
+      other_advise:o,
+      score:s,
+      time:time
+    }
+  })
+}
---
+++ miniprogram/pages/feedback/feedback.js
@@ -0,0 +1,125 @@
+// miniprogram/pages/feedback/feedback.js
+const app = getApp()
+var util = require('../../utils/util.js');
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    feedback_function:null,
+    feedback_usage:null,
+    feedback_other:null,
+    score:5
+  },
+  set_score: function(e) {
+    var score = parseInt(e.detail.value)
+    console.log(score)
+    this.data.score = score
+  },
+  bindinput_function: function(e) {
+    console.log(e.detail.value)
+    this.setData(
+      {
+        feedback_function: e.detail.value
+      }
+    )
+  },
+  bindinput_usage: function(e) {
+    console.log(e.detail.value)
+    this.setData(
+      {
+        feedback_usage: e.detail.value
+      }
+    )
+  },
+  bindinput_other: function(e) {
+    console.log(e.detail.value)
+    this.setData(
+      {
+        feedback_other: e.detail.value
+      }
+    )
+  },
+  submit: function(e) {
+    console.log(e)
+    var time = util.formatTime(new Date());
+    console.log(time)
+    wx.cloud.callFunction({
+      name:"add_feedback",
+      data: {
+        function_advise:this.data.feedback_function,
+        usage_advise:this.data.feedback_usage,
+        other_advise:this.data.feedback_other,
+        score: this.data.score,
+        time:time
+      },
+      success: res=> {
+        console.log("succ")
+      },
+      fail: err => {
+        console.log("error")
+      }
+    })
+    wx.navigateBack({
+      complete: (res) => {},
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
