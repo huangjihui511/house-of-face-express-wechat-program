dea1c764b0daf22b77484b92eac9242affc9b3a7
MyUserName <my_email@buaa.edu.cn>
2020-04-28 20:35:17 +0800

    add_load_solid

--- miniprogram/pages/team2/favorite_expression/index.js
+++ miniprogram/pages/team2/favorite_expression/index.js
@@ -7,11 +7,7 @@ return a+b
 }
 Page({
   data: {
-    kind:"",
-    divline_freq_color:"rgb(179, 175, 175)",
-    divline_kind_color:"rgb(179, 175, 175)",
-    freq_color:"rgb(179, 175, 175)",
-    kind_color:"rgb(179, 175, 175)",
+    color:"black",
     all_select:"",
     selected:"",
     select_text:"选择",
@@ -25,26 +21,64 @@ Page({
     images_view_srcs:[],
     can_delete_selected:[],
     delete_selected:[],
-    classes:['全部','公开','未公开','label2','label3','label4','label5','label6','label7']
+    classes:['全部','公开','未公开','label2','label3','label4','label5','label6','label7'],
+    navData:[
+      {
+          text: '全部'
+      },
+      {
+          text: '使用频率'
+      },
+      {
+          text: '公开'
+      },
+      {
+          text: '未公开'
+      },
+      {
+        text: 'label2'
+      },
+      {
+          text: 'label3'
+      },
+      {
+          text: 'label4'
+      },
+      {
+          text: 'label5'
+      },
+      {
+          text: 'label6'
+      },
+      {
+          text: 'label7'
+      }
+    ],
+    currentTab: 0,
+    navScrollLeft: 0
   },
-  bindPickerChange: function (e) {
-    let temp_divline_freq_color="divline_freq_color"
-    let temp_divline_kind_color="divline_kind_color"
-    let temp_freq_color="freq_color"
-    let temp_kind="kind"   
-    let temp_kind_color="kind_color"
+  switchNav(event){
+    if(this.data.button_select==true){
+      return
+    }
+    var cur = event.currentTarget.dataset.current; 
+    var singleNavWidth = this.data.windowWidth / 5;
     this.setData({
-      [temp_divline_freq_color]:"rgb(179, 175, 175)",
-      [temp_freq_color]:"rgb(179, 175, 175)",
-      [temp_divline_kind_color]:"rgb(76, 196, 126)",
-      [temp_kind]:this.data.classes[e.detail.value],
-      [temp_kind_color]:"black",
-    })
-    console.log('picker发送选择改变，携带下标为', e.detail.value)    
-    console.log('picker发送选择改变，携带值为', this.data.classes[e.detail.value])
-    this.label_select(this.data.classes[e.detail.value])
+        navScrollLeft: (cur - 2) * singleNavWidth
+    })      
+    if (this.data.currentTab == cur) {
+        return false;
+    } else {
+        this.setData({
+            currentTab: cur
+        })
+        if(cur==1){this.freq_order();}
+        else{
+          this.label_select(this.data.navData[cur].text)
+        }
+    }
   },
-    onShow: async function () {
+  onShow: async function () {
     var freq1=[];
     var label1=[];
     var images_src1=[];
@@ -90,10 +124,6 @@ Page({
     })
   },
   freq_order(){
-    if(this.data.button_select==true){
-      console.log("已禁用")
-      return;
-    }
     var freq=this.data.freqs
     var src=this.data.images_srcs
     var label=this.data.labels
@@ -117,16 +147,6 @@ Page({
         }
       }
      }
-     
-    let temp_kind="kind"   
-    let temp_kind_color="kind_color"
-    this.setData({
-      [temp_kind]:"",
-      [temp_kind_color]:"rgb(179, 175, 175)",
-    })
-    let temp_divline_freq_color="divline_freq_color"
-    let temp_divline_kind_color="divline_kind_color"
-    let temp_freq_color="freq_color"
     let temp_view_src = "images_view_srcs"
     let temp_src = "images_srcs"
     let temp_label = "labels"
@@ -136,11 +156,7 @@ Page({
       [temp_view_src]: src,
       [temp_label]: label,
       [temp_freq]: freq,
-      [temp_freq_color]:"black",
-      [temp_divline_kind_color]:"rgb(179, 175, 175)",
-      [temp_divline_freq_color]:"rgb(76, 196, 126)"
     })
-    console.log(this.data.freq_color)
   },
   delete_or_previewImage: function (e){
     if(this.data.can_delete_selected[e.currentTarget.dataset.index]==true){
@@ -236,6 +252,7 @@ Page({
         delete_selected_temp[i]=false
       }
       this.setData({
+        color: "rgb(218, 214, 214)",
         [temp_all_select]:"全选",
         [temp_select_number]:"已选择0张图片",
         [temp_button_select]:true,
@@ -251,6 +268,7 @@ Page({
         delete_selected_temp[i]=false
       }
       this.setData({
+        color: "black",
         [temp_all_select]:"",
         [temp_select_number]:"",
         [temp_button_select]:false,
